6d653547b82368a68a19d61e6e2b66cf6369be13
Allyster Costa <allystercosta@gmail.com>
2023-04-29 00:09:02 -0300

    Paginas base criadas, Fiz as paginas para cadastrar turmas Para o botão de seleção do coordenador do curso fiz uma recuperação usando o get do Jquery assim o coordenador será escolhido de acordo com o cadastro junto ao banco de dados.

---
+++ JS/ConexaoAoBD.js
@@ -0,0 +1,17 @@
+
+/* Esse arquivo será usado para aproveitar os locais em que necessario conexão com o BD */
+/* Banco de dados com metodo Fecth */
+
+const select = document.getElementById('selectDoBD');
+console.log('To aqui!');
+
+/* Aqui é utilizado a função GET do Jquery para recurar os dados do Banco
+e depois manipulando para que seja inserido dentro do HTML */
+$(document).ready(function() {
+    $.get('optionsDoSelect.php', function(data) {
+      // adiciona as opções do select
+      $('#selectDoBD').append(data);
+    });
+  });
+
+
---
+++ optionsDoSelect.php
@@ -0,0 +1,20 @@
+<?php
+/* Requer o arquivo de conexão */
+require('conexao.php');
+/* Inicia a conexão com o BD "sistema_academico" */
+mysqli_select_db($conexao,"sistema_academico");
+
+/* Aqui é feita a consulta para os nomes dos Professores cadastrados no banco */
+$consulta = 'SELECT nome FROM professores';
+$resultado = $conexao->query($consulta);
+
+while ($row = $resultado->fetch_assoc()) {
+    echo '<option value="' . $row['nome'] . '">' . $row['nome'] . '</option>';
+};
+
+
+mysqli_close($conexao);
+
+
+
+?>
